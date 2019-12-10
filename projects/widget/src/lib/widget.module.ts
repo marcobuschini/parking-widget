b6afca60f354fd4b24cd289ade65a1395b9be9bf
Marco Buschini <marco.buschini@gmail.com>
2019-12-10 20:25:34 +0100

    Changed configuration to build a package.
    Closes #1

--- projects/widget/src/lib/widget.module.ts
+++ projects/widget/src/lib/widget.module.ts
@@ -3,19 +3,19 @@ import { WidgetComponent } from './widget.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { HttpClientModule } from '@angular/common/http';
 import { MatCardModule } from '@angular/material/card';
-import { MatListItem, MatListModule } from '@angular/material/list';
+import { MatListModule } from '@angular/material/list';
 import { MatDividerModule } from '@angular/material/divider';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
   declarations: [WidgetComponent],
   imports: [
+    CommonModule,
     FlexLayoutModule,
     HttpClientModule,
     MatDividerModule,
     MatCardModule,
     MatListModule,
-    MatListItem,
-  ],
-  exports: [WidgetComponent]
+  ]
 })
 export class WidgetModule { }
