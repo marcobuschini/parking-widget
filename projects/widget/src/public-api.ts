2bf0df25eabca5bbdf890240e98fcff8b6489f2c
Marco Buschini <marco.buschini@gmail.com>
2019-12-15 15:12:38 +0100

    Exported the data classes from the widget.
    With this export applications using this widget can get the data structures by imporitng them from this component.

--- projects/widget/src/public-api.ts
+++ projects/widget/src/public-api.ts
@@ -5,3 +5,6 @@
 export * from './lib/widget.service';
 export * from './lib/widget.component';
 export * from './lib/widget.module';
+export * from './lib/feature';
+export * from './lib/parkingslot';
+export * from './lib/vendor';
